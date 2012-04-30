86ebaba8f5e43af46c8b83a2d015d35d5e9d2eec
Mario G. Pozzo <camboris@gmail.com>
2012-04-30 15:40:37 -0300

    commit inicial

---
+++ HaxePunkBasics/Source/HaxePunkBasics.hx
@@ -0,0 +1,41 @@
+package;
+
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
+import nme.display.Sprite;
+import nme.Lib;
+
+/**
+ * ...
+ * @author Boris
+ */
+class HaxePunkBasics extends Engine {
+	
+	public function new () {
+		
+		super (800,600, 60, false);
+		
+	}
+	
+	override public function init():Void {
+		#if debug
+			#if flash
+				if(flash.system.Capabilities.isDebugger)
+			#end
+				{
+					HXP.console.enable();
+				}
+		#end
+		HXP.screen.color = 0x333333;
+		HXP.screen.scale = 1;
+		
+		HXP.console.log(["Motor Inicializado"]);
+	}
+	
+	public static function main () {
+		
+		Lib.current.addChild (new HaxePunkBasics ());
+		
+	}	
+
+}
